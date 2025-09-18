054a6499883b6cc641a7ef3ad4acbf00195aba90
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2025-09-18 14:30:59 +0200

    nonparam hotfix

--- src_dev/engines/exptilt-nonparam/engine.R
+++ src_dev/engines/exptilt-nonparam/engine.R
@@ -48,19 +48,19 @@ exptilt_nonparam_engine <- function(
     max_iter=100,
     tol_value=1e-6
 ) {
-  json_path <- system.file("extdata", "method_params.json", package = "nmar")
+  # json_path <- system.file("extdata", "method_params.json", package = "nmar")
 
-  if (!file.exists(json_path)) {
-    stop("Method parameters JSON file (method_params.json) not found in 'inst/extdata/'. Package might be corrupted or not installed correctly.")
-  }
+  # if (!file.exists(json_path)) {
+  #   stop("Method parameters JSON file (method_params.json) not found in 'inst/extdata/'. Package might be corrupted or not installed correctly.")
+  # }
 
-  all_schemas <- jsonlite::read_json(json_path, simplifyVector = TRUE)
+  # all_schemas <- jsonlite::read_json(json_path, simplifyVector = TRUE)
 
   config <- list(
     # outcome_cols = outcome_cols,
     refusal_col = refusal_col,
-    max_iter = get_json_param_info(all_schemas, "exptilt_nonparam", "max_iter")$default,
-    tol_value = get_json_param_info(all_schemas, "exptilt_nonparam", "tol_value")$default
+    max_iter = 100, #hotfix
+    tol_value = 1e-6 #hotfix
   )
 
   # TODO: add validation
